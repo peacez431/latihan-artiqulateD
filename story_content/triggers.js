ca2fdc205d6f8264dbba7eb590702fffca7e4d4b
peacez431 <72952154+peacez431@users.noreply.github.com>
2026-03-04 09:50:22 +0800

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,74 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6O1ykDh0m5H":
+        Script1();
+        break;
+      case "6CFkGWcy2O2":
+        Script2();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+var getKeyDown = player.getKeyDown;
+var keydown = player.keydown;
+var keyup = player.keyup;
+window.Script1 = function()
+{
+  const target = object('68YNos9eMzF');
+const duration = 750;
+const easing = 'ease-out';
+const id = '5ibj4h4CW6c';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script2 = function()
+{
+  const target = object('6JiGL1ID681');
+const duration = 750;
+const easing = 'ease-out';
+const id = '5bNrBnCsWVz';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
